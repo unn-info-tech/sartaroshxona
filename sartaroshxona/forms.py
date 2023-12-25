2558df0342ead56a9a23ddee725d65cdee69f865
Nematulloh <unn.info.tech@gmail.com>
2023-12-25 11:45:35 +0300

    appointment written over again, deleted old one

--- sartaroshxona/forms.py
+++ sartaroshxona/forms.py
@@ -1,4 +1,13 @@
+# forms.py
+from django import forms
+from .models import Service, Barber
 
-# CLIENT
-        
-# MASTER
+class ServiceForm(forms.ModelForm):
+    class Meta:
+        model = Service
+        fields = '__all__'  # Use all fields from the Service model
+
+class BarberForm(forms.ModelForm):
+    class Meta:
+        model = Barber
+        fields = '__all__'  # Use all fields from the Barber model
--- sartaroshxona/views.py
+++ sartaroshxona/views.py
@@ -8,3 +8,39 @@ def sign(request):
 
 def test(request):
     return render(request, 'sartaroshxona/clients.html')
+
+# views.py
+from django.shortcuts import render, redirect
+from .models import Barber, Service
+from .forms import BarberForm, ServiceForm
+
+def barber_profile(request):
+    barber = Barber.objects.get(user=request.user)  # Fetch the logged-in barber's data
+
+    if request.method == 'POST':
+        if 'barber_info_form' in request.POST:  # Update barber information
+            barber_form = BarberForm(request.POST, instance=barber)
+            if barber_form.is_valid():
+                barber_form.save()
+                return redirect('barber_profile')
+
+        elif 'service_form' in request.POST:  # Add new service
+            service_form = ServiceForm(request.POST)
+            if service_form.is_valid():
+                service = service_form.save(commit=False)
+                service.master = barber  # Associate the service with the logged-in barber
+                service.save()
+                return redirect('barber_profile')
+
+    else:
+        barber_form = BarberForm(instance=barber)
+        service_form = ServiceForm()
+
+    services = barber.get_services()  # Fetch services associated with the barber
+
+    return render(request, 'sartaroshxona/prof.html', {
+        'barber': barber,
+        'barber_form': barber_form,
+        'services': services,
+        'service_form': service_form
+    })
