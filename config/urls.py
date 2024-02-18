9926bbe67c7b13b221ba0c2ee0f6e7f0d13193b3
Nematulloh <unn.info.tech@gmail.com>
2024-02-18 11:40:16 +0300

    shortcomings

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
-
+from django.views.static import serve
 
 urlpatterns = [
     path('secret/boshqaruv_of_unn/', admin.site.urls),
@@ -27,12 +27,11 @@ urlpatterns = [
     path('main/', include('main.urls')),
     path('loc/', include('locations.urls')),
 
-] 
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
 
-
-# Serve uploaded media files during development
+# Serve uploaded media files and statics during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-
