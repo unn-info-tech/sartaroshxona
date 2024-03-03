9352edec3b59161993c4491516d394c9a84492a1
Nematulloh <unn.info.tech@gmail.com>
2024-03-03 13:48:47 +0300

    redirect to after sign in

--- main/utils.py
+++ main/utils.py
@@ -3,8 +3,8 @@ from django.shortcuts import redirect
 def middleware(request):
     if request.user.is_authenticated:
         if request.user.is_barber:  # Assuming is_barber is a boolean field in your User model
-            return redirect('barber_profile')  # Redirect to the barber profile URL
+            return redirect('appointments_by_category', category='in_queue')  # Redirect to the barber profile URL
         else:
-            return redirect('client_profile')
+            return redirect('barbers_list')
     else:
         return redirect('signin')  # Redirect to login page if user is not logged in
